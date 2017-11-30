61bcec52f455443b217597269039df4c8ed85a6f
Nathan Harrison <nathan.andrew.harrison@gmail.com>
2017-11-29 21:32:11 -0500

    Fixed unit test

--- src/test/java/com/nah/math/Point2DTest.java
+++ src/test/java/com/nah/math/Point2DTest.java
@@ -11,7 +11,6 @@ public class Point2DTest {
 		Point2D p2 = new Point2D(-1.0, -1.0);
 		assertEquals(p1.getDistance(p2), 5.0, 1e-6); // 3rd arg is tolerence (delta)
 		assertEquals(p2.getDistance(p1), 5.0, 1e-6);
-		assertEquals(p2.getDistance(p1), 1.234, 1e-6);
 	}
 
 }
