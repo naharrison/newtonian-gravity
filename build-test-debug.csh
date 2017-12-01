5afeabcacf60294009f0cffb7db0be026ffa0d40
Nathan Harrison <nathan.andrew.harrison@gmail.com>
2017-12-01 14:40:23 -0500

    Added unit tests

--- build-test-debug.csh
+++ build-test-debug.csh
@@ -1,9 +1,21 @@
 #!/bin/csh -f
 
 rm -rf ~/.m2
+
 mvn install
+
+if($? != 0) then
+	echo "Crap! mvn install failed"
+	exit 1
+endif
+
 #mvn com.github.spotbugs:spotbugs-maven-plugin:spotbugs # spotbugs goal produces a report target/spotbugsXml.xml
 mvn com.github.spotbugs:spotbugs-maven-plugin:check # check goal produces a report and produces build failed if bugs
 # the spotbugsXml.xml file is easiest read in a web browser
 # see http://spotbugs.readthedocs.io/en/latest/maven.html and https://spotbugs.github.io/spotbugs-maven-plugin/index.html for more info
+
+if($? != 0) then
+	echo "Crap! mvn com.github.spotbugs:spotbugs-maven-plugin:check failed"
+	exit 1
+endif
 
--- src/test/java/com/nah/math/Point3DTest.java
+++ src/test/java/com/nah/math/Point3DTest.java
@@ -13,4 +13,28 @@ public class Point3DTest {
 		assertEquals(p2.getDistance(p1), 13.0, 1e-6);
 	}
 
+	@Test
+	public void testGetRadius() {
+		Point3D p1 = new Point3D(3.0, 4.0, 12.0);
+		assertEquals(p1.getRadius(), 13.0, 1e-6);
+	}
+
+	@Test
+	public void testGetThetaInDegrees() {
+		Point3D p1 = new Point3D(3.0, 4.0, 12.0);
+		assertEquals(p1.getThetaInDegrees(), Math.toDegrees(Math.atan2(5.0, 12.0)), 1e-6);
+	}
+
+	@Test
+	public void testGetThetaInRadians() {
+		Point3D p1 = new Point3D(3.0, 4.0, 12.0);
+		assertEquals(p1.getThetaInRadians(), Math.atan2(5.0, 12.0), 1e-6);
+	}
+
+	@Test
+	public void testGetPhiInDegrees() {
+		Point3D p1 = new Point3D(3.0, 4.0, 12.0);
+		assertEquals(p1.getPhiInDegrees(), Math.toDegrees(Math.atan2(4.0, 3.0)), 1e-6);
+	}
+
 }
