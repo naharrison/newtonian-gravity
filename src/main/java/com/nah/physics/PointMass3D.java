5d1e99cf2bded130b125bf653d3b4930575dbbdb
Nathan Harrison <nathan.andrew.harrison@gmail.com>
2017-12-01 14:46:48 -0500

    Added new class

---
+++ src/main/java/com/nah/physics/PointMass3D.java
@@ -0,0 +1,15 @@
+package com.nah.physics;
+
+import com.nah.math.Point3D;
+
+public class PointMass3D {
+	
+	public Point3D position;
+	public double mass;
+	
+	public PointMass3D(Point3D position, double mass) {
+		this.position = position;
+		this.mass = mass;
+	}
+
+}
