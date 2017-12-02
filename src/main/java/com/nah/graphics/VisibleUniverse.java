d7b1f1b3951ce0d13ae0eb5121a2b5b1b612e917
Nathan Harrison <nathan.andrew.harrison@gmail.com>
2017-12-02 04:31:41 -0500

    More progress

--- src/main/java/com/nah/graphics/VisibleUniverse.java
+++ src/main/java/com/nah/graphics/VisibleUniverse.java
@@ -8,8 +8,8 @@ import com.nah.physics.PointMass2D;
 public class VisibleUniverse extends JFrame {
 
 	public Universe2D universe;
-	public int maxJFrameDimension = 800; // pixels
-	public int marginSize = 25; // pixels
+	public int maxJFrameDimension = 900; // pixels
+	public int marginSize = 50; // pixels
 
 	public VisibleUniverse(Universe2D universe) {
 		super();
@@ -33,15 +33,14 @@ public class VisibleUniverse extends JFrame {
 	public void paint(Graphics g) {
 		g.setColor(Color.blue);
 		for(PointMass2D mass : universe.masses) {
+			int radius = 10; // pixels
 			double xpixel = (mass.position.x - universe.getDimensions().xstart)/universe.getDimensions().xwidth;
 			xpixel = xpixel*(getWidth() - 2.0*marginSize) + marginSize;
-			xpixel = xpixel - Math.sqrt(2.0)*10.0; // to shift to center (instead of top left) (10 = radius)
 			double ypixel = (mass.position.y - universe.getDimensions().ystart)/universe.getDimensions().ywidth;
 			ypixel = ypixel*(getHeight() - 2.0*marginSize) + marginSize;
 			ypixel = getHeight() - ypixel;
-			ypixel = ypixel - Math.sqrt(2.0)*10.0;
-			g.fillOval((int) xpixel, (int) ypixel, 20, 20);
-			System.out.println(xpixel + " " + ypixel);
+			g.fillOval((int) (xpixel - Math.sqrt(2.0)*radius), (int) (ypixel - Math.sqrt(2.0)*radius), 2*radius, 2*radius); // to shift to center (instead of top left)
+			g.drawLine((int) xpixel, (int) ypixel, (int) xpixel, (int) (ypixel + 25));
 		}
 	}
 
